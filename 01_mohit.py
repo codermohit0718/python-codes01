14ac184a5451bb2db23fa837e2f3599c527499f6
codermohit0718 <codermohit0718@gmail.com>
2025-11-20 14:53:01 +0530

    Add files via upload

---
+++ 01_mohit.py
@@ -0,0 +1,8 @@
+def sub(a,b):
+    sub = a-b
+    return sub
+num_1= int(input("enter a number a:"))
+num_2 = int(input("enter a number a:"))
+result = sub(num_1,num_2)
+print("the sub is:",result)
+    
