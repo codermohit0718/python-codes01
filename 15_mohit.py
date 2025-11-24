cc65549982562c292f4fd7fa8df870576449fe0e
codermohit0718 <codermohit0718@gmail.com>
2025-11-24 21:30:37 +0530

    Add files via upload

---
+++ 15_mohit.py
@@ -0,0 +1,10 @@
+# Program to compare two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print(a, "is greater than", b)
+elif b > a:
+    print(b, "is greater than", a)
+else:
+    print("Both numbers are equal")
