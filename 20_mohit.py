5f5819848d65ad9da1db419c5484f2508b3b8092
codermohit0718 <codermohit0718@gmail.com>
2025-11-24 21:46:48 +0530

    Add files via upload

---
+++ 20_mohit.py
@@ -0,0 +1,9 @@
+# Program to create a dictionary of student details
+student = {
+    "name": "Mohit",
+    "age": 18,
+    "course": "Python Programming"
+}
+
+print("Student Dictionary:")
+print(student)
