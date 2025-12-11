65a68eeb65d2c4f65b93e87cb7aa75257f530187
codermohit0718 <codermohit0718@gmail.com>
2025-12-11 15:12:48 +0530

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+iris = pd.read_csv(r"c:\Users\mohit\Downloads\iris.csv")
+print(iris)
+print(iris.head())
+print(iris.tail())
+print(iris.describe())
+print 
